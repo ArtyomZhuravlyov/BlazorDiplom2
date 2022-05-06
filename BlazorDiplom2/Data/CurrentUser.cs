9c75e7c065903063c34c41ca3c8749a4b5412952
ArtyomZhuravlyov <44862635+ArtyomZhuravlyov@users.noreply.github.com>
2022-05-06 16:54:48 +0300

    Сделаны список курсов, изменение и добавление курсов

---
+++ BlazorDiplom2/Data/CurrentUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorDiplom2.Data
+{
+    public static class CurrentUser
+    {
+        //public static LocalStorage LocalStorage { get; set; }
+
+        public static AspNetUsers User { get; set; }
+
+        public static async void SetUserAsync(AuthenticationStateProvider authenticationStateProvider, UserManager<AspNetUsers> _userManager)
+        {
+            var authstate = await authenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authstate.User;
+            User = await _userManager.GetUserAsync(user);
+        }
+    }
+}
