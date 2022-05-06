f03ec2fcc90822db31fb8469c68b6f68be328bb7
ArtyomZhuravlyov <44862635+ArtyomZhuravlyov@users.noreply.github.com>
2022-05-06 14:04:28 +0300

    Сделаны компоненты с добавлением групп

--- BlazorDiplom2/Data/Group.cs
+++ BlazorDiplom2/Data/Group.cs
@@ -1,11 +1,18 @@
-﻿namespace BlazorDiplom2.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDiplom2.Data
 {
     public class Group
     {
         public int Id { get; set; }
-        public string Name { get; set; } 
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
 
         public int? EducationalInstitutionId { get; set; }
+
 
         public EducationalInstitution? EducationalInstitution { get; set; }
         public List<Teacher> Teachers { get; set; } = new List<Teacher>();
