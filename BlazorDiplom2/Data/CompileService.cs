a5e2b6c01ee6314361b4ddfe30f58441c0b464d4
ArtyomZhuravlyov <44862635+ArtyomZhuravlyov@users.noreply.github.com>
2022-05-11 16:28:05 +0300

    Убрал Async и чуть-чуть переделал задание кода для Монако

--- BlazorDiplom2/Data/CompileService.cs
+++ BlazorDiplom2/Data/CompileService.cs
@@ -14,52 +14,22 @@ namespace BlazorDiplom2.Data
 {
     public class CompileService
     {
-        //public HttpClient Http { get; set; }
 
-        //public  NavigationManager UriHelper { get; set; }
 
         public List<string> CompileLog { get; set; }
-        private List<MetadataReference> references { get; set; }
+
+        private List<MetadataReference> _references;
 
         public void Init()
         {
-            if (references == null)
+            if (_references == null)
             {
-                references = new List<MetadataReference>();
+                _references = new List<MetadataReference>();
 
                 string[] libs = Directory.GetFiles($"{Directory.GetCurrentDirectory()}\\wwwroot\\Libs\\");
 
                 foreach (var lib in libs)
-                    references.Add(MetadataReference.CreateFromFile(lib));
-
-               
-                
-
-                //references.Add(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
-                //references.Add(MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location));
-                //references.Add(MetadataReference.CreateFromFile(@"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\6.0.4\System.Runtime.dll"));
-
-                //references.Add(MetadataReference.CreateFromFile(AppDomain.CurrentDomain.BaseDirectory + "//xunit.abstractions.dll"));
-                //references.Add(MetadataReference.CreateFromFile(AppDomain.CurrentDomain.BaseDirectory + "//xunit.assert.dll"));
-                //references.Add(MetadataReference.CreateFromFile(AppDomain.CurrentDomain.BaseDirectory + "//xunit.core.dll"));
-                //references.Add(MetadataReference.CreateFromFile(AppDomain.CurrentDomain.BaseDirectory + "//xunit.execution.dotnet.dll"));
-
-                //var a = AppDomain.CurrentDomain.GetAssemblies();
-                //foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-                //{
-                //    if (assembly.IsDynamic)
-                //    {
-                //        continue;
-                //    }
-                //    var name = assembly.GetName().Name + ".dll";
-                //    Console.WriteLine(name);
-                //    Console.WriteLine(UriHelper.BaseUri);
-                //    Console.WriteLine(UriHelper.BaseUri + "/_framework/_bin/" + name);
-
-                //    references.Add(
-                //        MetadataReference.CreateFromStream(
-                //            await Http.GetStreamAsync(UriHelper.BaseUri + "/_framework/_bin/" + name)));
-                //}
+                    _references.Add(MetadataReference.CreateFromFile(lib));
             }
         }
 
@@ -78,13 +48,17 @@ namespace BlazorDiplom2.Data
                     {
                         var methodInfo = type.GetMethod("Run");
                         if (methodInfo != null)
+                        {
                             dict.Add(methodInfo, type);
+                            if (dict.Count > 1)
+                                return ("Найдено больше одной точки входа", false);
+                        }
+
                     }
 
-                    if(dict.Count == 0)
+                    if (dict.Count == 0)
                         return ("Не найдена точка входа(метод Run)", false);
-                    else if(dict.Count > 1)
-                        return ("Найдено больше одной точки входа", false);
+
 
                     var instance = Activator.CreateInstance(dict.First().Value);
                     dict.First().Key.Invoke(instance, null);
@@ -101,7 +75,6 @@ namespace BlazorDiplom2.Data
 
         public async Task<Assembly> Compile(string code)
         {
-            //await InitAsync();
 
             SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code, new CSharpParseOptions(LanguageVersion.Preview));
             foreach (var diagnostic in syntaxTree.GetDiagnostics())
@@ -114,11 +87,11 @@ namespace BlazorDiplom2.Data
                 CompileLog.Add("Parse SyntaxTree Error!");
                 return null;
             }
-            
+
             CompileLog.Add("Parse SyntaxTree Success");
 
             CSharpCompilation compilation = CSharpCompilation.Create("CompileBlazorInBlazor.Demo", new[] { syntaxTree },
-                references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+                _references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
 
             using (MemoryStream stream = new MemoryStream())
             {
@@ -137,17 +110,14 @@ namespace BlazorDiplom2.Data
 
                 CompileLog.Add("Compilation success!");
 
-                //stream.Seek(0, SeekOrigin.Begin);
-
                 //                var context = new CollectibleAssemblyLoadContext();
                 Assembly assembly = AppDomain.CurrentDomain.Load(stream.ToArray());
                 return assembly;
             }
 
-            return null;
         }
 
 
-     
+
     }
 }
